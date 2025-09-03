b5b65b0fb323c71c7bf38215f6e4231e396a6139
Julian_Lucas <j.lucas1429@gmail.com>
2025-09-03 19:38:10 -0400

    Add files via upload

---
+++ assignments/01_getting_started/01_01_python_org.py
@@ -0,0 +1,8 @@
+# I have been looking through python.org
+# I found the following to be interesting
+# I appreciate that Python is complety free
+# I also like that there are many free Python books
+# Python is a multi-paradigm programming language
+# Python's name is based on the TV show Monty Pyhton's Flying Circus 
+# Python frequently uses English keywords where other languages use punctuation
+# Python uses whitespace indentation, rather than curly braces or keywords, to delimit blocks
