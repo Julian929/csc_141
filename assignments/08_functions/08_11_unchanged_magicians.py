7408adc89e3cdc0cca965169c56116153172a041
Julian_Lucas <j.lucas1429@gmail.com>
2025-10-30 11:29:44 -0400

    Update 08_11_unchanged_magicians.py

--- assignments/08_functions/08_11_unchanged_magicians.py
+++ assignments/08_functions/08_11_unchanged_magicians.py
@@ -1,1 +1,21 @@
-print
+def show_messages(messages):
+    """Print text messages."""
+    for message in messages:
+        print(message)
+
+# show_messages(messages)
+
+def send_messages(message, sent_messages):
+    """Print messages and move to list"""
+    while send_messages:
+        message = messages.pop()
+        print(message)
+        sent_messages.append(message)
+
+messages = ['Hi', ';How are you?', 'Hello there!']
+send_messages = []
+
+send_messages(messages[:], sent_messages)
+print(messages)
+print(sent_messages)
+
