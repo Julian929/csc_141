f9706b7de88bda4e41eb4d2474503edeca7ce378
Julian_Lucas <j.lucas1429@gmail.com>
2025-11-05 15:40:50 -0500

    Update 10_06_programming_poll.py

---
+++ assignments/10_files_and_exceptions/10_06_programming_poll.py
@@ -0,0 +1,8 @@
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+
+try:
+    result = int(num1) + int(num2)
+    print(f"The result is: {result}")
+except ValueError:
+    print("Wrong value provided. Enter two numbers.")
